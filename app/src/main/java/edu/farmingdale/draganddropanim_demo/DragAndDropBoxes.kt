6cb51f22c6d6d4a4ec9f4d50bc0a99945d61fb88
Jason Devaraj <jdevaraj01@gmail.com>
2025-11-26 13:21:32 -0500

    Rectangle also rotates when the button is pressed.

--- app/src/main/java/edu/farmingdale/draganddropanim_demo/DragAndDropBoxes.kt
+++ app/src/main/java/edu/farmingdale/draganddropanim_demo/DragAndDropBoxes.kt
@@ -16,29 +16,14 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.draganddrop.dragAndDropSource
 import androidx.compose.foundation.draganddrop.dragAndDropTarget
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.filled.Face
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draganddrop.DragAndDropEvent
@@ -47,18 +32,13 @@ import androidx.compose.ui.draganddrop.DragAndDropTransferData
 import androidx.compose.ui.draganddrop.mimeTypes
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ViewAgenda
 import androidx.compose.material.icons.filled.CameraAlt
 import androidx.compose.material3.Button
 
-
-// Drag and drop composable
 @Composable
 fun DragAndDropBoxes(modifier: Modifier = Modifier) {
     var isPlaying by remember { mutableStateOf(true) }
@@ -71,9 +51,7 @@ fun DragAndDropBoxes(modifier: Modifier = Modifier) {
                 .weight(0.2f)
         ) {
             val boxCount = 4
-            var dragBoxIndex by remember {
-                mutableIntStateOf(0)
-            }
+            var dragBoxIndex by remember { mutableIntStateOf(0) }
 
             repeat(boxCount) { index ->
                 Box(
@@ -84,8 +62,7 @@ fun DragAndDropBoxes(modifier: Modifier = Modifier) {
                         .border(1.dp, Color.Black)
                         .dragAndDropTarget(
                             shouldStartDragAndDrop = { event ->
-                                event
-                                    .mimeTypes()
+                                event.mimeTypes()
                                     .contains(ClipDescription.MIMETYPE_TEXT_PLAIN)
                             },
                             target = remember {
@@ -117,13 +94,10 @@ fun DragAndDropBoxes(modifier: Modifier = Modifier) {
                                 .fillMaxSize()
                                 .dragAndDropSource {
                                     detectTapGestures(
-                                        onLongPress = { offset ->
+                                        onLongPress = {
                                             startTransfer(
                                                 transferData = DragAndDropTransferData(
-                                                    clipData = ClipData.newPlainText(
-                                                        "text",
-                                                        ""
-                                                    )
+                                                    clipData = ClipData.newPlainText("text", "")
                                                 )
                                             )
                                         }
@@ -141,8 +115,7 @@ fun DragAndDropBoxes(modifier: Modifier = Modifier) {
         )
 
         val rtatView by animateFloatAsState(
-            targetValue = if (isPlaying) 360f else 0.0f,
-            // Configure the animation duration and easing.
+            targetValue = if (isPlaying) 360f else 0f,
             animationSpec = repeatable(
                 iterations = if (isPlaying) 10 else 1,
                 tween(durationMillis = 3000, easing = LinearEasing),
@@ -164,9 +137,12 @@ fun DragAndDropBoxes(modifier: Modifier = Modifier) {
                     .size(100.dp)
             )
 
-            MyButton {
-                position = IntOffset(400, 100)
-            }
+            MyButton(
+                onReset = {
+                    position = IntOffset(400, 100)
+                    isPlaying = !isPlaying
+                }
+            )
         }
     }
 }
